7856f93b17e78dd0062dc419c4b971195ef3c801
David G. Fonnegra <fonne@mattersupply.co>
2023-09-08 20:52:04 -0500

    Changing multiplication with addition

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -4,9 +4,9 @@ package co.edu.eafit.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero * numero;
+        return numero + numero;
     }
-    
+
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
